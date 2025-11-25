af83af5c64324500ce6de022b04f8bb25085bc2c
waweakweak <vonnannilov1031@gmail.com>
2025-11-26 04:40:38 +0800

    Fix widget CSS typo: w3 -> w-3

--- user/Smodulepart.php
+++ user/Smodulepart.php
@@ -1993,7 +1993,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     <h3 class="text-sm font-bold text-gray-800">Eye Tracking</h3>
                 </div>
                 <div id="tracking-status-indicator" class="flex items-center gap-1">
-                    <span class="relative flex h-3 w3">
+                    <span class="relative flex h-3 w-3">
                         <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-green-400 opacity-75"></span>
                         <span class="relative inline-flex rounded-full h-3 w-3 bg-green-500"></span>
                     </span>
